f6e634b3c23b7fb9abf9e7c9dd5eae34a4f1c69b
liobarrozo <liobarrozo8@gmail.com>
2025-07-22 04:17:06 -0300

    avances seccion usos

---
+++ js/index.js
@@ -0,0 +1,32 @@
+lottie.loadAnimation({
+    container: document.getElementById('icono-data'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/data.json' // animación para apps
+  });
+  
+  lottie.loadAnimation({
+    container: document.getElementById('icono-learn'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/learn.json' // animación educativa
+  });
+  
+  lottie.loadAnimation({
+    container: document.getElementById('icono-web'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/web.json' // datos abiertos
+  });
+  
+  lottie.loadAnimation({
+    container: document.getElementById('icono-setup'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'assets/setup.json' // prototipado
+  });
+  
